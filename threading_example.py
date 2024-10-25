edca7b59e65f7a7951a555e28139586870855b28
Tao Xiang <xiang.tao@outlook.de>
2024-10-25 21:22:53 +0800

    1

--- threading_example.py
+++ threading_example.py
@@ -1,7 +1,9 @@
 import leopy.leoroutine as lrt
+import time
 
 # 使用示例
-def some_job(x, y):
+def some_job(x, y,t):
+    time.sleep(t)
     return x + y
 
 def on_job_done(result):
@@ -9,5 +11,10 @@ def on_job_done(result):
 
 if __name__ == "__main__":
     manager = lrt.ThreadManager()
-    manager.start_new_job(some_job,x=1,y= 2, callback=on_job_done)
+    manager.start_new_job(some_job,x=1,y= 2, t=9,callback=on_job_done)
+    time.sleep(1)
+    manager.start_new_job(some_job,x=1,y= 3, t=3,callback=on_job_done)
+    time.sleep(1)
+    manager.start_new_job(some_job,x=1,y= 4,t=2, callback=on_job_done)
+    time.sleep(3)
     manager.wait_all_jobs()
